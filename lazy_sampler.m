c51390254b30b61f5c2bbcad25e4d78bf0ddea57
Viktor <viktor.eklind@gmail.com>
2018-04-03 12:00:28 -0400

    simpler way to get the time vector associated with a sampled vector

---
+++ lazy_sampler.m
@@ -0,0 +1,10 @@
+function [time_vector,sample_period] = lazy_sampler(data,fs)
+    % data is a 1D sampled data vector, fs is the used sample frequency.
+    % Returns the time vector assoicated with the sample data, sample
+    % time period
+    
+    n = length(data);
+    sample_period = 1/fs;
+    end_time = n*sample_period-sample_period;
+    time_vector = 0:sample_period:end_time;
+end

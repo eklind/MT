74f70a443979639ce8c528b5161701993d3eee5d
Viktor <viktor.eklind@gmail.com>
2018-04-06 11:56:33 -0400

    exploring noise

--- test.m
+++ test.m
@@ -1,73 +1,38 @@
-clear all
-clc
-load noisysignals s1 s2
-t=0:0.01:100;
+%% 
 
-A=sin(t)+0.4;
-B=sin(t-400)-0.2;
+Fs = 1000;            % Sampling frequency                    
+T = 1/Fs;             % Sampling period       
+L = 1500;             % Length of signal
+t = (0:L-1)*T;        % Time vector
+
+S = 0.7*sin(2*pi*50*t) + sin(2*pi*120*t);
+X = S + 2*randn(size(t));
+
+plot(1000*t(1:50),X(1:50))
+title('Signal Corrupted with Zero-Mean Random Noise')
+xlabel('t (milliseconds)')
+ylabel('X(t)')
+
+%% FT
+Y = fft(X,L);
+f = Fs*(0:(L-1))/L;
+
+
+% Two sided and single-sided spectrum
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
 
 
 
-hold on
-plot(t,A)
-plot(t,B)
 
-X=s1;
-Y=s2;
-
-%%
-  %%Get the peak power before filtering
-    
-    % Get data
-%     d = energy.data;
-
-    % Remove mean
-%     d = d - ones(length(d),1)*mean(d);
-%     x = X'-ones(length(X),1)*mean(X);
-    x = X-mean(X);
-    y = Y-mean(Y);
-    % Sum over three phases I*V
-%     P = sum(d(:,2:4).*d(:,5:7),2);
-   %% 
-    %%Filter
-    
-    % Filter window size
-    N = 1000;
-%     P = conv(P, ones(N,1),'valid')/N;
-
-    x = conv(x, ones(N,1),'valid')/N;
-
-    % Filter trace data
-%     d = trace;
-   % d(sum(isnan(d),2)>0,:) = [];
-
-%     x = d(:,2:7);
-
-%     dx = [zeros(1,6);diff(x)];
-
-%     P = downsample(P,10000*0.012);
-%%
-    % Synch data
-%     f = xcorr(P,sum(dx.^2,2));
-    f = xcorr(x,y);
-    
-    [acor,lag] = xcorr(x,y);
-    [~,I] = max(abs(acor));
-    timeDiff = abs(lag(I));
-    %%
-    plot(X(timeDiff:end))
-    hold on
-    plot(Y)
-    
-    
-    %%
-    [~,f] = max(f);
-%%
-    x(1:f-length(x)) = [];
-    x = x(1:length(y));
 
 %%
+K=2*0.157/0.454/39.37; %lbs/inch
+L=300/25.4; %inch
+F=70;   %hz
 
-hold on
-plot(t,X)
-plot(t,Y)
+T=0.0104*K*L^2*F^2 %lbs
+
+
+

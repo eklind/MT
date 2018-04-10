94291675da3e7b1a0293aacd788456ad6dfc1d13
JonathanKarlsson92 <jonathan.karlsson13@gmail.com>
2018-04-10 10:36:56 -0400

    script to obtain frequency components of a signal ver3

--- fft_analyse.m
+++ fft_analyse.m
@@ -1,28 +1,32 @@
-function [fft_data,f,pks,freq] = fft_analyse(data,hz,show_freq,numPeaks)
+function [fft_data,f] = fft_analyse(data,hz,show_freq)
 
-L=length(data);
-%data_filt=outlier_detection(data,0);
-%data_filt=detrend(data_filt); %remove?
+
+data_filt=outlier_detection(data,0);
+data_filt=detrend(data_filt); %remove?
 %data_filt=smoothdata(data_filt);
+L=length(data);
 
 %frequency vector and abs fft data
+%L=length(data);
 f = hz*(0:(L/2))/L;
 f=f';
 fft_data=abs(fft(data_filt,L))/L;
 
-if(nargin>3)
-    peaks=numPeaks;
-else
-    peaks=200;
-end
-[pks,locs] = findpeaks(fft_data(1:length(f)),'MinPeakDistance',peaks);
-freq=f(locs);
+%L=hz/2;
+%f=linspace(0,hz/2,L);
+%f=f';
+%fft_data=abs(fft(data_filt,L))/L;
+
+%peaks=200;
+
+%[pks,locs] = findpeaks(fft_data(1:length(f)),'MinPeakDistance',peaks);
+%freq=f(locs);
 if(show_freq==true)
-    plot(f,fft_data(1:length(f)))
+    stem(f(2:length(f)/2),fft_data(2:length(f)/2))
     hold on
-    plot(f(locs),fft_data(locs),'r*')
+    %plot(f(locs),fft_data(locs),'r*')
     ylabel('Magnitude')
-    tlt=strcat('Frequency domain of vibration signal, sampled at: ',num2str(hz),'hz')
+    tlt=strcat('Frequency domain of vibration signal, sampled at: ',num2str(hz),'hz');
     title(tlt)
     xlabel('Frequency')
     legend('Vibration data')

70f0782a60fd29a68e692f504a8ce17bc57c965e
JonathanKarlsson92 <jonathan.karlsson13@gmail.com>
2018-04-10 11:11:38 -0400

    script to obtain frequency components of a signal ver4

--- fft_analyse.m
+++ fft_analyse.m
@@ -1,38 +1,36 @@
-function [fft_data,f] = fft_analyse(data,hz,show_freq)
-
+function [fft_data_n,f] = fft_analyse(data,hz,show_freq,downsample)
+%returns frequency data up to Nyquist frequency
+%plots data if third input==true
 
 data_filt=outlier_detection(data,0);
-data_filt=detrend(data_filt); %remove?
-%data_filt=smoothdata(data_filt);
+%data_filt=detrend(data_filt); 
 L=length(data);
 
 %frequency vector and abs fft data
-%L=length(data);
 f = hz*(0:(L/2))/L;
 f=f';
 fft_data=abs(fft(data_filt,L))/L;
+fft_data_n=fft_data(1:length(f));
 
-%L=hz/2;
-%f=linspace(0,hz/2,L);
-%f=f';
-%fft_data=abs(fft(data_filt,L))/L;
-
-%peaks=200;
-
-%[pks,locs] = findpeaks(fft_data(1:length(f)),'MinPeakDistance',peaks);
-%freq=f(locs);
 if(show_freq==true)
-    stem(f(2:length(f)/2),fft_data(2:length(f)/2))
+    subplot(2,1,1)
+    stem(f,fft_data(1:length(f)))
     hold on
-    %plot(f(locs),fft_data(locs),'r*')
     ylabel('Magnitude')
     tlt=strcat('Frequency domain of vibration signal, sampled at: ',num2str(hz),'hz');
     title(tlt)
     xlabel('Frequency')
     legend('Vibration data')
-    
     hold off
+    
+    subplot(2,1,2)
+    f2=f(1:downsample:end);
+    for(i=0:length(f2)-1)
+        fft_data2(i+1)=sum(fft_data(1+i*downsample:(i+1)*downsample));
+    end
+    stem(f2,fft_data2(1:length(f2)))
+    ylabel('Magnitude')
+    xlabel('Frequency')
+    legend('Vibration data frequencies collected')
 end
-
-
 end

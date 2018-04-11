f6946c2130d41834498d8465e11977045f9263b0
JonathanKarlsson92 <jonathan.karlsson13@gmail.com>
2018-04-11 11:00:57 -0400

    modified freq

--- fft_analyse.m
+++ fft_analyse.m
@@ -1,6 +1,7 @@
-function [fft_data_n,f] = fft_analyse(data,hz,show_freq,downsample)
+function [fft_data,f] = fft_analyse(data,hz,show_freq,number)
 %returns frequency data up to Nyquist frequency
 %plots data if third input==true
+%if number(even) in entered, scales the fft to that amount of values
 
 data_filt=outlier_detection(data,0);
 %data_filt=detrend(data_filt); 
@@ -10,11 +11,25 @@ L=length(data);
 f = hz*(0:(L/2))/L;
 f=f';
 fft_data=2*abs(fft(data_filt,L))/(L);
-fft_data_n=fft_data(1:length(f));
+%fft_data_n=fft_data(1:length(f));
 
 if(show_freq==true)
-    subplot(2,1,1)
-    stem(f,fft_data(1:length(f)))
+    
+    if(nargin>3)
+        num_plot=3;
+        fft_data2=2*abs(fft(data_filt,number))/(number);
+        f2=(hz/length(fft_data2))*(0:length(fft_data2)/2);
+        
+        %prepare output
+        f=f2(2:end);
+        fft_data=fft_data2(2:number/2+1); 
+    else
+        num_plot=2;
+    end
+    
+    %plot1
+    subplot(1,num_plot,1)
+    stem(f(2:end),fft_data(2:length(f)));
     hold on
     ylabel('Magnitude')
     tlt=strcat('Frequency domain of vibration signal, sampled at: ',num2str(hz),'hz');
@@ -23,16 +38,16 @@ if(show_freq==true)
     legend('Vibration data')
     hold off
     
-    if(nargin>3)
-        subplot(2,1,2)
-        f2=f(1:downsample:end);
-        for(i=0:length(f2)-1)
-            fft_data2(i+1)=sum(fft_data(1+i*downsample:(i+1)*downsample));
-        end
-        stem(f2,fft_data2(1:length(f2)))
+    %plot2
+        subplot(1,num_plot,2)
+        stem(f2(2:end),fft_data2(2:length(f2)));
         ylabel('Magnitude')
         xlabel('Frequency')
         legend('Vibration data frequencies collected')
-    end
+     
+    %plot3
+    subplot(1,num_plot,3)
+    bar(f,fft_data);
+    
 end
 end

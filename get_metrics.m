177167411b97e3e9c944aa4a6c1a5ce06b8b65a8
JonathanKarlsson92 <jonathan.karlsson13@gmail.com>
2018-04-12 15:22:23 -0400

    function to store metrics of time series data

--- get_metrics.m
+++ get_metrics.m
@@ -2,13 +2,42 @@ function [out,removed]=get_metrics(data,window_size)
 %takes data of dimension m x 1 and outputs a struct with key values
 %returns removed number of values
 %[size_1,size_2]=size(data);
+
 L=length(data);
-removed=mod(L,window_size);
-data=data(1:end-removed,1); %remove for even windowing
-for i=1:1
-out.reshaped=reshape(data,[length(data)/window_size window_size])
+if(nargin>1)
+    removed=mod(L,window_size);
+    data=data(1:end-removed,1); %remove for even windowing
+else
+    removed=0;
+    window_size=L;
+end
+    
+%Reshape
+out.reshaped=reshape(data,[length(data)/window_size window_size])'
+
+%Statistical moments, time domain
 out.mean=mean(out.reshaped); %first moment of shunks
 out.var=var(out.reshaped); %second moment of shunks
 out.skew=skewness(out.reshaped); %third moment of shunks
 out.kurtosis=kurtosis(out.reshaped); %fourth moment of shunks
-end
+
+%trends
+%out.trend_linear=diff(data-detrend(data));
+out.std=std(out.reshaped);
+%rms value
+out.rms=rms(out.reshaped);
+
+%moving median
+%out.movmad=movmad(out.reshaped,10);
+
+%envelope
+[out.env_u,out.env_l]=envelope(out.reshaped);
+
+%approx entropy
+out.entropy=approximateEntropy(out.reshaped);
+
+%bandwidth
+out.bw= powerbw(out.reshaped)
+
+%pkurtosis
+out.pkurtosis=kurtosis(out.reshaped);

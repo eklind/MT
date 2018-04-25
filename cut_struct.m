b1a578193d2b857fe3b9cff1a36d0d618974bcf6
JonathanKarlsson92 <jonathan.karlsson13@gmail.com>
2018-04-25 16:53:41 -0400

    added function cut_struct to extract a part of a generated struct

---
+++ cut_struct.m
@@ -0,0 +1,22 @@
+function struct_short=cut_struct(struct,s_start,s_stop)
+%returns part of struct
+%input struct, start index and stop index, scales for HF
+struct_short=struct;
+LF=struct2cell(struct.LF);
+LF=cell2mat(LF(2:end));
+HF=struct2cell(struct.HF);
+HF=cell2mat(HF(2:end));
+namesLF=fieldnames(struct.LF);
+namesHF=fieldnames(struct.HF);
+
+N=size(LF,1)
+M=size(HF,1)
+for i=1:N
+struct_short.LF.(namesLF{i+1})=LF(i,s_start:s_stop);
+end
+
+for i=1:M
+struct_short.HF.(namesHF{i+1})=HF(i,10000*(s_start-1)+1:10000*s_stop);    
+end
+
+end

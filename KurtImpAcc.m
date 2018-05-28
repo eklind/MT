af3573735ba775c64b07617332d8564d02c415da
Viktor <viktor.eklind@gmail.com>
2018-05-28 12:33:34 -0400

    improvements

--- KurtImpAcc.m
+++ KurtImpAcc.m
@@ -1,5 +1,5 @@
 function kurt = KurtImpAcc(t,compStatus, disP, accz)
-%Takes time, compressor status and compressor discharege pressure and accelerometer data
+%Takes HF time, compressor status and compressor discharege pressure and accelerometer data
 % and calculates the kurtosis value of the impulse response. 
 % Assumes fs=1Hz for disharge pressure
 fs=1/mean(diff(t));
@@ -14,19 +14,31 @@ e=3*fs;%Time to monitor the impulse respones [s]
 
 %Find the time index when the compressor discharge pressure is lowest
 % and the extract the corresponding accelerometer data
-for i=1:N
-    t1=comp_on(i)-t_before;
-    t2=comp_on(i)+t_after;
-    [~,start_time] = min(disP(t1:t2)); 
-    start_time = (t1+start_time-2)*fs;
-    impAcc(:,i) = accz(start_time:(start_time+e));
+if(sum(comp_on)>=1)
+    for i=1:N
+        t1=comp_on(i)-t_before;
+        t2=comp_on(i)+t_after;
+        [~,start_time] = min(disP(t1:t2)); 
+        start_time = (t1+start_time-1)*fs;
+        impAcc(:,(i)) = accz(start_time:(start_time+e));
+    end
+    kurt = kurtosis(impAcc); %[1xN]
+    kurt=kurt-3;
+    kurt=kurt.^2;
+else
+    kurt=0;
+end
+    
+
+
+
 end
 
-kurt = kurtosis(impAcc); %[1xN]
-kurt=kurt-3;
-kurt=kurt.^2;
-end
 
 
-%% 
+
+
+
+
+
 
